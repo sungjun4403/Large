4eea9751e168c6170f1951ea783e6e5aaa059f68
sungjun4403 <geulligu89@naver.com>
2022-11-10 05:45:33 +0900

    .

--- large_f/src/main.js
+++ large_f/src/main.js
@@ -1,8 +1,4 @@
 import { createApp } from 'vue'
 import NavBar from './NavBar.vue'
-import Vue from '../node_modules/vue'
-import router from './router'
 
-Vue.use(router)
-
-createApp(NavBar).use(router).mount('#NavBar')
+createApp(NavBar).mount('#NavBar')
--- large_f/vue.config.js
+++ large_f/vue.config.js
@@ -1,9 +1,8 @@
-
-
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
 module.exports = {
   devServer: {
     proxy: {
