6d46f0ac4ef07f09d3cca82418dd077d142c3105
sungjun4403 <geulligu89@naver.com>
2022-12-19 14:27:04 +0900

    .

--- large_b/src/main/java/com/project/large/gitAPI/controller/gitController.java
+++ large_b/src/main/java/com/project/large/gitAPI/controller/gitController.java
@@ -3,6 +3,7 @@ package com.project.large.gitAPI.controller;
 import com.project.large.gitAPI.service.GitService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -15,10 +16,9 @@ public class gitController {
 
     @GetMapping("/getcode")
     public void getCode() throws IOException {
-
-
         String path = "sungjun4403/O_baksa_go/main/OBSpjct/OBSapp/views.py";
         List<String> getCode = gitService.getSourceFile(path);
-
     }
+
+    @PostMapping("/")
 }
