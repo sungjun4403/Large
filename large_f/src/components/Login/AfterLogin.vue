367919ebd8d4f6e6fabfce4c497753e2793216ad
sungjun4403 <geulligu89@naver.com>
2023-02-12 23:16:14 +0900

    .

--- large_f/src/components/Login/AfterLogin.vue
+++ large_f/src/components/Login/AfterLogin.vue
@@ -18,16 +18,16 @@ export default {
         passCode() {
             const url = window.location.href
             const code = url.split("=")[1]
-            axios.get("http://localhost:8080/auth/github/callback?code=" + code).then((response) => {
+            axios.get("https://large-devlog.com/auth/github/callback?code=" + code).then((response) => {
                 if (response.status == 200) { //로그인 성공시 홈으로 리다이렉트                    
                     localStorage.setItem("AccessToken", response.headers["accesstoken"])
                     localStorage.setItem("memberToken", response.headers["membertoken"])
                     localStorage.setItem("gitID", response.headers["gitid"])
                     
-                    window.location.href = "http://localhost:3000/" + response.headers["gitid"]
+                    window.location.href = "https://large-devlog.com/" + response.headers["gitid"]
                 }
                 else { //로그인 실패시 LoginFailed로 리다이렉트 
-                    window.location.href = "http://localhost:3000/login_failed"
+                    window.location.href = "https://large-devlog.com/login_failed"
                 }
             }) 
         },
